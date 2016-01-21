c7db3d7388d07dde7cfa5427ebbd54a31b743e68
Анна Комова <komova@skobeeff.ru>
2016-01-21 13:56:27 +0300

    news and sliders

--- src/assets/toolkit/scripts/components/owl.carousel.custom.js
+++ src/assets/toolkit/scripts/components/owl.carousel.custom.js
@@ -8,4 +8,75 @@ $(function() {
 	  items: 1
 	});
 
+	$('.carousel--two-items').owlCarousel({
+	  	loop:true,
+	  	nav:true,
+  		responsive:{
+	        320:{
+	            items:1
+	        },
+	        640:{
+	            items:1
+	        },
+	        960:{
+	            items:2
+	        },
+	        1200:{
+	            items:2
+	        },
+	        1344:{
+	            items:2
+	        }
+	    }
+	});
+
+	$('.carousel--four-items').owlCarousel({
+	  	loop:true,
+	  	nav:true,
+  		responsive:{
+	        320:{
+	            items:1
+	        },
+	        640:{
+	            items:2
+	        },
+	        960:{
+	            items:4
+	        },
+	        1200:{
+	            items:4
+	        },
+	        1344:{
+	            items:4
+	        }
+	    }
+	});
+
+	$('.news008__slider').owlCarousel({
+	  	loop:true,
+	  	margin:0,
+	  	nav:true,
+	    navText: [
+	      "<i class='demo-icon icon-basic-arrow-left'></i>",
+	      "<i class='demo-icon icon-basic-arrow-right'></i>"
+	      ],
+	  	responsive:{
+	        320:{
+	            items:1
+	        },
+	        640:{
+	            items:1
+	        },
+	        960:{
+	            items:2
+	        },
+	        1200:{
+	            items:2
+	        },
+	        1344:{
+	            items:3
+	        }
+	    }
+	});
+
 });
