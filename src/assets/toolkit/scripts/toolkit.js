ae883206c3953d0c38b8c2725a2eaf61e6e0c8fe
Анна Комова <anna@devgroup.ru>
2016-02-02 13:48:17 +0300

    header

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -12,4 +12,5 @@ require('./components/owl.carousel.custom');
 require('./components/popup.custom');
 require('./components/chosen.custom');
 require('./forms/form--006');
+require('./header/header--001');
 
