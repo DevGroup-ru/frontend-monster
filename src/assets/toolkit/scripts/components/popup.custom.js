3f7c2b533e00e8862db203b930217ba8f19bb37b
Анна Комова <anna@devgroup.ru>
2016-04-19 17:25:50 +0300

    popup fix

--- src/assets/toolkit/scripts/components/popup.custom.js
+++ src/assets/toolkit/scripts/components/popup.custom.js
@@ -1,5 +1,29 @@
 $(function() {
   	"use strict";
+  	$.fn.extend({
+  		destroyPopup: function($popupOpenElement) {
+  			var $el = $(this);
+            var popupBackground = $('#' + $el.attr('id') + '_background');            
+            var $wrapper = $('#' + $el.attr('id') + '_wrapper');
+
+            // remove background
+            if (popupBackground) {
+                popupBackground.remove();
+            }
+
+            // remove click listener
+            if ($popupOpenElement) {
+                $(document).off('click', $popupOpenElement);
+            }
+
+            // remove wrapper
+            $wrapper.remove();
+
+            // remove initialization marker to allow popup to be initialize again
+            $el.data('popup-initialized', false);
+  		}
+  		/// $("#my_popup").destroyPopup();
+  	});
 
   	$('.popup-button').click(function(){
 	    var dataBModal = $(this).attr('data-button');
@@ -10,12 +34,15 @@ $(function() {
 	    	$item.attr('id', dataBModal).popup({
 			  	transition: 'all 0.3s 0s',
 			  	beforeopen: function() {
-				   $(".popup_wrapper").addClass("popup-alone-wrap");
+				   $(".popup_wrapper").addClass("m-wrapper popup-alone-wrap");
 				}
 			});	
 		} else {
 	        $item.attr('id', dataBModal).popup({
-			  	transition: 'all 0.3s 0s'
+			  	transition: 'all 0.3s 0s',
+			  	beforeopen: function() {
+				   $(".popup_wrapper").addClass("m-wrapper");
+				}
 			});		
 		}
 	});	
@@ -29,21 +56,24 @@ $(function() {
 	        $item.attr('id', dataBModal).popup({
 	        	transition: 'all 0.3s',
 	        	beforeopen: function() {
-				   $(".popup_wrapper").addClass("popup-alone-wrap");
+				   $(".popup_wrapper").addClass("m-wrapper popup-alone-wrap");
 				},
 				closetransitionend: function() {
 				   $("." + dataBModal + "_open").after($item);
 				   $item.removeAttr('style');
-				   $item.attr('id', dataBModal).popup('destroy');
+				   $item.attr('id', dataBModal).destroyPopup();
 				}
 			});
 		} else {
 			$item.attr('id', dataBModal).popup({
 	        	transition: 'all 0.3s',
+	        	beforeopen: function() {
+				   $(".popup_wrapper").addClass("m-wrapper");
+				},
 				closetransitionend: function() {
 				   $("." + dataBModal + "_open").after($item);
 				   $item.removeAttr('style');
-				   $item.attr('id', dataBModal).popup('destroy');
+				   $item.attr('id', dataBModal).destroyPopup();
 				}
 			});
 		}
