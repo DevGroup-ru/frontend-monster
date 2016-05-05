1b34677a386d8eccb7a244450c402fe1b9921ec3
fps01 <fps.06@mail.ru>
2016-05-05 13:02:06 +0300

    VisualFrame. Added basic actions

--- src/assets/toolkit/scripts/builder/VisualFrame.js
+++ src/assets/toolkit/scripts/builder/VisualFrame.js
@@ -1,44 +1,63 @@
 import FrameApi from './../utils/FrameApi';
 
-class VisualFrame {
-  constructor() {
-    this.params();
-    this.initialize();
-  }
-
-  initialize() {
-    FrameApi.bindMessageListener(this);
-
-    this.parentWindow = window.parent;
-    /** @var FrontendMonster */
-    this.parentMonster = this.parentWindow.FrontendMonster;
-    this.currentMonsterContent = false;
-    this.makeItMove();
-  }
-
-  get $monsterContent() {
-    if (this.$monsterContentCache) {
-      return this.$monsterContentCache;
+class VisualFrame
+{
+    constructor()
+    {
+        this.params();
+        this.initialize();
     }
 
-    this.refreshMonsterContentCache();
+    initialize()
+    {
+        FrameApi.bindMessageListener(this);
+        this.parentWindow = window.parent;
+        /** @var FrontendMonster */
+        this.parentMonster = this.parentWindow.FrontendMonster;
+        this.currentMonsterContent = false;
+        this.makeItMove();
+    }
 
-    return this.$monsterContentCache;
-  }
+    get $monsterContent()
+    {
+        if (this.$monsterContentCache) {
+            return this.$monsterContentCache;
+        }
+        this.refreshMonsterContentCache();
+        return this.$monsterContentCache;
+    }
 
-  refreshMonsterContentCache() {
-    this.$monsterContentCache = {};
-    const that = this;
-    $(this.settings['monster-content-selector']).each(function iter() {
-      if (!that.currentMonsterContent) {
-        that.currentMonsterContent = $(this).data('uniqueContentId');
-      }
-      that.$monsterContentCache[$(this).data('uniqueContentId')] = $(this);
-    });
-  }
+    refreshMonsterContentCache()
+    {
+        this.$monsterContentCache = {};
+        const that = this;
+        $(this.settings['monster-content-selector']).each(function iter() {
+            if (!that.currentMonsterContent) {
+                that.currentMonsterContent = $(this).data('uniqueContentId');
+            }
+            that.$monsterContentCache[$(this).data('uniqueContentId')] = $(this);
+        });
+    }
 
-  makeItMove() {
-    this.$handlers = $(`
+    getNewMaterialIndex()
+    {
+        if (!this.lastMaterialIndex) {
+            var lastIndex = 0;
+            $('[data-is-material]').each(function () {
+                var index = $(this).data('material-index');
+                if (index > lastIndex) {
+                    lastIndex = index;
+                }
+            });
+            this.lastMaterialIndex = lastIndex;
+        }
+        this.lastMaterialIndex++;
+        return this.lastMaterialIndex;
+    }
+
+    makeItMove()
+    {
+        this.$handlers = $(`
 <div class="monster-block-handlers">
     <a href="#" class="monster-block-handlers__configure">
         <i class="fa fa-cog"></i>
@@ -57,102 +76,144 @@ class VisualFrame {
         <i class="fa fa-times"></i>
     </a>
 </div>`);
-    $('body').append(this.$handlers);
-    this.$handlers.hide();
-    const that = this;
-    $(this.settings['monster-content-selector']).on({
-      mouseenter: function hoverIn() {
-        const $this = $(this);
-        that.$handlers.show();
-        $this.addClass('m-monster-content__material--highlighted');
-      },
-      mouseleave: function hoverOut() {
-        const $this = $(this);
-        $this.removeClass('m-monster-content__material--highlighted');
-      },
-      click: function clickHandler() {
-        const $this = $(this);
-        that.selectMaterial($this);
-      },
-    }, '[data-is-material]');
-  }
-
-  selectMaterial($material) {
-    if (this.$selectedMaterial === $material) {
-      return;
+        $('body').append(this.$handlers);
+        this.$handlers.hide();
+        const that = this;
+        $(this.settings['monster-content-selector']).on({
+                mouseenter: function hoverIn() {
+                const $this = $(this);
+                that.$handlers.show();
+                $this.addClass('m-monster-content__material--highlighted');
+            },
+            mouseleave: function hoverOut() {
+                const $this = $(this);
+                $this.removeClass('m-monster-content__material--highlighted');
+            },
+                click: function clickHandler() {
+                const $this = $(this);
+                that.selectMaterial($this);
+            }
+        }, '[data-is-material]');
+        that.$handlers.on('click', '.monster-block-handlers__move-up', function() {
+            if (that.$selectedMaterial) {
+                var $prev = that.$selectedMaterial.prev('[data-is-material]');
+                if ($prev.length == 1) {
+                    that.$selectedMaterial.insertBefore($prev);
+                    that.selectMaterial(that.$selectedMaterial, true);
+                }
+            }
+            return false;
+        }).on('click', '.monster-block-handlers__move-down', function() {
+            if (that.$selectedMaterial) {
+                var $next = that.$selectedMaterial.next('[data-is-material]');
+                if ($next.length == 1) {
+                    that.$selectedMaterial.insertAfter($next);
+                    that.selectMaterial(that.$selectedMaterial, true);
+                }
+            }
+            return false;
+        }).on('click', '.monster-block-handlers__clone', function() {
+            if (that.$selectedMaterial) {
+                var $clonedMaterial = that.$selectedMaterial.clone();
+                $clonedMaterial.data('material-index', that.getNewMaterialIndex()).insertAfter(that.$selectedMaterial);
+                that.selectMaterial($clonedMaterial);
+            }
+            return false;
+        }).on('click', '.monster-block-handlers__remove', function() {
+            if (that.$selectedMaterial) {
+                if (confirm('Are you sure you want to remove this material?')) {
+                    that.$selectedMaterial.remove();
+                    that.$selectedMaterial = null;
+                    that.$handlers.hide(); // it does not work. why? Need to fix!
+                }
+            }
+            return false;
+        });
     }
 
-    if (this.$selectedMaterial) {
-      this.$selectedMaterial.removeClass('m-monster-content__material--active');
+    selectMaterial($material, force = false)
+    {
+        if (this.$selectedMaterial === $material && force === false) {
+            return;
+        }
+
+        if (this.$selectedMaterial) {
+            this.$selectedMaterial.removeClass('m-monster-content__material--active');
+        }
+        // align handlers
+        this.$handlers.css('top', $material.position().top + $material.height() - this.$handlers.height());
+        $material.addClass('m-monster-content__material--active');
+        this.$selectedMaterial = $material;
     }
-    // align handlers
-    this.$handlers.css('top', $material.position().top + $material.height() - this.$handlers.height());
-    $material.addClass('m-monster-content__material--active');
-    this.$selectedMaterial = $material;
-  }
 
-  serializeContent(callback) {
-    const result = {};
-    const that = this;
-    for (const uniqueContentId in this.$monsterContent) {
-      if (this.$monsterContent.hasOwnProperty(uniqueContentId)) {
-        const $monster = this.$monsterContent[uniqueContentId];
-        result[$monster.data('uniqueContentId')] = that.serializeUniqueContent($monster);
-      }
+    serializeContent(callback)
+    {
+        const result = {};
+        const that = this;
+        for (const uniqueContentId in this.$monsterContent) {
+            if (this.$monsterContent.hasOwnProperty(uniqueContentId)) {
+                const $monster = this.$monsterContent[uniqueContentId];
+                result[$monster.data('uniqueContentId')] = that.serializeUniqueContent($monster);
+            }
+        }
+        this.sendToBuilder(callback, [result]);
     }
-    this.sendToBuilder(callback, [result]);
-  }
 
-  serializeUniqueContent($monsterContent) {
-    const result = {};
-    result.uniqueContentId = $monsterContent.data('uniqueContentId');
-    result.materials = {};
-    $monsterContent.find('[data-is-material=\'1\']').each(function iter() {
-      const material = {};
-      material.block = $(this).data('materialBlock');
-      result.materials[$(this).data('materialIndex')] = material;
-    });
-    return result;
-  }
-  /**
-   * Sets VisualFrame settings.
-   * Uses VisualFrameSettings variable if provided or default values instead.
-   */
-  params() {
-    const userSettings = window.VisualFrameSettings || {};
-    const settings = {
-      'monster-content-selector': '.m-monster-content',
-    };
-    for (const key in userSettings) {
-      if (userSettings.hasOwnProperty(key)) {
-        settings[key] = userSettings[key];
-      }
+    serializeUniqueContent($monsterContent)
+    {
+        const result = {};
+        result.uniqueContentId = $monsterContent.data('uniqueContentId');
+        result.materials = {};
+        $monsterContent.find('[data-is-material=\'1\']').each(function iter() {
+            const material = {};
+            material.block = $(this).data('materialBlock');
+            result.materials[$(this).data('materialIndex')] = material;
+        });
+        return result;
     }
-    this.settings = settings;
-  }
 
-  sendToBuilder(func, args) {
-    FrameApi.sendMessage(this.parentWindow, func, args);
-  }
+    /**
+     * Sets VisualFrame settings.
+     * Uses VisualFrameSettings variable if provided or default values instead.
+     */
+    params()
+    {
+        const userSettings = window.VisualFrameSettings || {};
+        const settings = {
+            'monster-content-selector': '.m-monster-content'
+        };
+        for (const key in userSettings) {
+            if (userSettings.hasOwnProperty(key)) {
+                settings[key] = userSettings[key];
+            }
+        }
+        this.settings = settings;
+    }
 
-  newBlock(blockName, newBlockUrl) {
-    const that = this;
-    $.ajax({
-      url: newBlockUrl,
-      method: 'POST',
-      cache: false,
-      data: {
-        block: blockName,
-        uniqueContentId: this.currentMonsterContent,
-        materialIndex: that.$monsterContent[that.currentMonsterContent].find('[data-is-material=\'1\']').length,
-      },
-    }).done(function ok(data) {
-      const $element = $(data);
-      that.$monsterContent[that.currentMonsterContent].append($element);
-      /* global smoothScroll:false */
-      smoothScroll.animateScroll($element[0].offsetTop);
-    });
-  }
+    sendToBuilder(func, args)
+    {
+        FrameApi.sendMessage(this.parentWindow, func, args);
+    }
+
+    newBlock(blockName, newBlockUrl)
+    {
+        const that = this;
+        $.ajax({
+            url: newBlockUrl,
+            method: 'POST',
+            cache: false,
+            data: {
+                block: blockName,
+                uniqueContentId: this.currentMonsterContent,
+                materialIndex: that.$monsterContent[that.currentMonsterContent].find('[data-is-material=\'1\']').length
+            }
+        }).done(function ok(data) {
+            const $element = $(data);
+            that.$monsterContent[that.currentMonsterContent].append($element);
+            /* global smoothScroll:false */
+            smoothScroll.animateScroll($element[0].offsetTop);
+        });
+    }
 }
 
 export default VisualFrame;
